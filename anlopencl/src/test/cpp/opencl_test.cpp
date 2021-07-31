36fbf1338591635003358707d5869fe80ad8bd61
Erwin Müller <erwin.mueller@deventm.org>
2021-07-31 09:11:49 +0200

    Feature #4483 Use c++ bindings for opencl

--- anlopencl/src/test/cpp/opencl_test.cpp
+++ anlopencl/src/test/cpp/opencl_test.cpp
@@ -20,11 +20,16 @@
 #include <CL/cl.h>
 #endif
 
-using namespace std;
+#define CL_HPP_ENABLE_EXCEPTIONS
+#include <CL/cl2.hpp>
+
+#include "program_ex.hpp"
+
+using namespace cl;
 using ::testing::TestWithParam;
 using ::testing::Values;
 
-typedef vector<cl_mem> (*createBuffer)(cl_context);
+typedef std::vector<cl_mem> (*createBuffer)(cl_context);
 
 struct OpenCL_Context {
 	string kernel;
@@ -34,45 +39,26 @@ struct OpenCL_Context {
 
 class OpenCL_Context_Fixture: public ::testing::TestWithParam<OpenCL_Context> {
 public:
-	cl_uint availablePlatforms;
-	cl_uint availableDevices;
-	cl_platform_id platform_id = NULL;
-	cl_device_id device_id = NULL;
-	cl_context context = NULL;
-	cl_command_queue queue = NULL;
-	vector<cl_mem> memobjs;
-	cl_program program = NULL;
-	cl_kernel kernel = NULL;
-	size_t *global_work_offset;
-	size_t *global_work_size;
-	size_t *local_work_size;
-	vector<string> libraries;
-	char** librariesStrs;
+	std::vector<string> input_headers;
+	std::vector<string> input_header_names;
 protected:
 	OpenCL_Context_Fixture() { // @suppress("Class members should be properly initialized")
-		libraries.push_back("src/main/cpp/use_opencl.h");
-		libraries.push_back("src/main/cpp/opencl_utils.h");
-		libraries.push_back("src/main/cpp/utility.h");
+		input_headers.push_back(readFile("src/main/cpp/use_opencl.h"));
+		input_header_names.push_back("use_opencl.h");
+		input_headers.push_back(readFile("src/main/cpp/opencl_utils.h"));
+		input_header_names.push_back("opencl_utils.h");
+		input_headers.push_back(readFile("src/main/cpp/utility.h"));
+		input_header_names.push_back("utility.h");
 	};
 
-	void printBuildLog() {
-		size_t log_size;
-		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-				0, NULL, &log_size);
-
-		// Allocate memory for the log
-		char *log = (char*) malloc(log_size);
-
-		// Get the log
-		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-				log_size, log, NULL);
-
-		// Print the log
-		printf("%s\n", log);
+	static std::string readFile(std::string fileName) {
+		std::ifstream f(fileName);
+		std::string s((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+		return s;
 	}
 
-	vector<const char*> strlist(vector<string> &input) {
-	    vector<const char*> result;
+	std::vector<const char*> strlist(std::vector<string> &input) {
+	    std::vector<const char*> result;
 	    // remember the nullptr terminator
 	    result.reserve(input.size()+1);
 	    transform(begin(input), end(input),
@@ -83,161 +69,121 @@ protected:
 	    return result;
 	}
 
+	bool loadPlatform() {
+	    std::vector<Platform> platforms;
+	    Platform::get(&platforms);
+	    Platform plat;
+	    for (auto &p : platforms) {
+	        std::string platver = p.getInfo<CL_PLATFORM_VERSION>();
+	        if (platver.find("OpenCL 3.") != std::string::npos) {
+	            plat = p;
+	        }
+	    }
+	    if (plat() == 0)  {
+	        std::cout << "No OpenCL 3 platform found.";
+	        return false;
+	    }
+	    Platform newP = Platform::setDefault(plat);
+	    if (newP != plat) {
+	        std::cout << "Error setting default platform.";
+	        return false;
+	    }
+	    return true;
+	}
+
 	virtual void SetUp() {
-		cl_int err;
-		clGetPlatformIDs(1, &platform_id, &availablePlatforms);
-		EXPECT_GT(availablePlatforms, 0) << "No OpenCL platform available.";
-		clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &availableDevices);
-		EXPECT_GT(availableDevices, 0) << "No OpenCL device available.";
-		context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &err);
-		if (err != CL_SUCCESS) {
-			switch (err) {
-				case CL_INVALID_PLATFORM:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_INVALID_PLATFORM";
-					break;
-				case CL_INVALID_PROPERTY:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_INVALID_PROPERTY";
-					break;
-				case CL_INVALID_VALUE:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_INVALID_VALUE";
-					break;
-				case CL_INVALID_DEVICE_TYPE:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_INVALID_DEVICE_TYPE";
-					break;
-				case CL_DEVICE_NOT_AVAILABLE:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_DEVICE_NOT_AVAILABLE";
-					break;
-				case CL_DEVICE_NOT_FOUND:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_DEVICE_NOT_FOUND";
-					break;
-				case CL_OUT_OF_RESOURCES:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_OUT_OF_RESOURCES";
-					break;
-				case CL_OUT_OF_HOST_MEMORY:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error: CL_OUT_OF_HOST_MEMORY";
-					break;
-				default:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create context error";
-					break;
+		EXPECT_TRUE(loadPlatform()) << "Unable to load platform";
+		vector<Program> programs;
+		vector<Program> input_headers_p;
+		for (auto s : input_headers) {
+			Program p(s);
+			try {
+				std::cerr << s << std::endl << std::endl;
+				p.compile();
+			} catch (...) {
+				 cl_int buildErr = CL_SUCCESS;
+				 auto buildInfo = p.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&buildErr);
+				 for (auto &pair : buildInfo) {
+				 	std::cerr << pair.second << std::endl << std::endl;
+				 }
 			}
+			input_headers_p.push_back(p);
 		}
-
-		cl_device_id devices[1];
-		clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, devices, NULL);
-		queue = clCreateCommandQueueWithProperties(context, devices[0], 0, &err);
-		EXPECT_EQ(err, 0) << "Create command queue error.";
-
-		librariesStrs = new char*[libraries.size() + 1];
-		int i;
-		for (i = 0; i < libraries.size(); i++) {
-			auto library = libraries[i];
-			ifstream f(library.c_str());
-			string s((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
-			librariesStrs[i] = new char[s.size() + 1];
-			strcpy(librariesStrs[i], s.c_str());
-		}
-
 		auto t = GetParam();
-		librariesStrs[i] = new char[t.source.size() + 1];
-		strcpy(librariesStrs[i], t.source.c_str());
+		ProgramEx psource(t.source);
+		psource.compile(input_headers_p, input_header_names);
+		programs.push_back(psource);
+		Program pfinal = linkProgram(programs);
 
-		program = clCreateProgramWithSource(context, i, (const char**)librariesStrs, NULL, &err);
-		if (err != CL_SUCCESS) {
-			switch (err) {
-				case CL_INVALID_CONTEXT:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create program error: CL_INVALID_CONTEXT";
-					break;
-				case CL_INVALID_VALUE:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create program error: CL_INVALID_VALUE";
-					break;
-				case CL_OUT_OF_RESOURCES:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create program error: CL_OUT_OF_RESOURCES";
-					break;
-				case CL_OUT_OF_HOST_MEMORY:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create program error: CL_OUT_OF_HOST_MEMORY";
-					break;
-				default:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create program error";
-					break;
-			}
+		int numElements = 64;
+	    std::vector<int> output(numElements, 0xdeadbeef);
+	    cl::Buffer outputBuffer(begin(output), end(output), false);
+	    cl::Pipe aPipe(sizeof(cl_int), numElements / 2);
+
+	    cl::DeviceCommandQueue defaultDeviceQueue;
+        defaultDeviceQueue = cl::DeviceCommandQueue::makeDefault();
+
+
+	    auto vectorAddKernel =
+	        cl::KernelFunctor<
+	            cl::Buffer,
+	            cl::DeviceCommandQueue
+	        >(pfinal, "updateGlobal");
+
+	    cl_int error;
+    	vectorAddKernel(
+            cl::EnqueueArgs(
+                cl::NDRange(numElements/2),
+                cl::NDRange(numElements/2)),
+            outputBuffer,
+            defaultDeviceQueue,
+    		error
+        );
+
+    	 cl::copy(outputBuffer, begin(output), end(output));
+
+		cl::Device d = cl::Device::getDefault();
+		std::cout << "Max pipe args: " << d.getInfo<CL_DEVICE_MAX_PIPE_ARGS>() << "\n";
+		std::cout << "Max pipe active reservations: " << d.getInfo<CL_DEVICE_PIPE_MAX_ACTIVE_RESERVATIONS>() << "\n";
+		std::cout << "Max pipe packet size: " << d.getInfo<CL_DEVICE_PIPE_MAX_PACKET_SIZE>() << "\n";
+		std::cout << "Device SVM capabilities: " << d.getInfo<CL_DEVICE_SVM_CAPABILITIES>() << "\n";
+		std::cout << "\tCL_DEVICE_SVM_COARSE_GRAIN_BUFFER = " << CL_DEVICE_SVM_COARSE_GRAIN_BUFFER << "\n";
+		std::cout << "\tCL_DEVICE_SVM_FINE_GRAIN_BUFFER = " << CL_DEVICE_SVM_FINE_GRAIN_BUFFER << "\n";
+		std::cout << "\tCL_DEVICE_SVM_FINE_GRAIN_SYSTEM = " << CL_DEVICE_SVM_FINE_GRAIN_SYSTEM << "\n";
+		std::cout << "\tCL_DEVICE_SVM_ATOMICS = " << CL_DEVICE_SVM_ATOMICS << "\n";
+
+		auto v = pfinal.getInfo<CL_PROGRAM_BINARIES>();
+		auto v2 = pfinal.getInfo<CL_PROGRAM_BINARY_SIZES>();
+		std::vector<std::vector<unsigned char>> v3;
+		std::vector<size_t> v4;
+		pfinal.getInfo(CL_PROGRAM_BINARIES, &v3);
+		pfinal.getInfo(CL_PROGRAM_BINARY_SIZES, &v4);
+
+		std::cout << "Binaries: " << v.size() << "\n";
+		std::cout << "Binary sizes: " << v2.size() << "\n";
+		for (size_t s : v2) {
+			std::cout << "\t" << s << "\n";
 		}
 
-		err = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-		if (err != CL_SUCCESS) {
-			printBuildLog();
-			switch (err) {
-			case CL_INVALID_PROGRAM:
-				EXPECT_EQ(err, CL_SUCCESS)
-						<< "Build program error: CL_INVALID_PROGRAM";
-				break;
-			case CL_BUILD_PROGRAM_FAILURE:
-				EXPECT_EQ(err, CL_SUCCESS)
-						<< "Build program error: CL_BUILD_PROGRAM_FAILURE";
-				break;
-			default:
-				EXPECT_EQ(err, CL_SUCCESS) << "Build program error";
-				break;
-			}
-		}
-
-		kernel = clCreateKernel(program, t.kernel.c_str(), &err);
-		if (err != CL_SUCCESS) {
-			switch (err) {
-				default:
-					EXPECT_EQ(err, CL_SUCCESS) << "Create kernel error";
-					break;
-			}
-		}
-
-		size_t global_work_size[1] = { 256 };
-		global_work_size[0] = 1024;
-		local_work_size[0] = 64; //Nvidia: 192 or 256
-		clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_work_size,
-				local_work_size, 0, NULL, NULL);
-
-		err = clFinish(queue);
-		if (err != CL_SUCCESS) {
-			switch (err) {
-				default:
-					EXPECT_EQ(err, CL_SUCCESS) << "Finish error";
-					break;
-			}
+		std::cout << "Output:\n";
+		for (int i = 1; i < numElements; ++i) {
+			std::cout << "\t" << output[i] << "\n";
 		}
 	}
 
 	virtual void TearDown() {
-		for (int i = 0; i < libraries.size() + 1; i++) {
-			const char* s = librariesStrs[i];
-			delete s;
-		}
-		delete librariesStrs;
-		for (cl_mem m : memobjs) {
-			clReleaseMemObject(m);
-		}
-		if (kernel != NULL) {
-			clReleaseKernel(kernel);
-		}
-		if (program != NULL) {
-			clReleaseProgram(program);
-		}
-		if (queue != NULL) {
-			clReleaseCommandQueue(queue);
-		}
-		if (context != NULL) {
-			clReleaseContext(context);
-		}
 	}
 };
 
 TEST_P(OpenCL_Context_Fixture, opencl_value_noise2D) {
 	auto t = GetParam();
-	EXPECT_GT(availableDevices, 0);
+	//EXPECT_GT(availableDevices, 0);
 	//EXPECT_FLOAT_EQ(value_noise2D(t.x, t.y, t.seed, t.interp), t.noise);
 }
 
-vector<cl_mem> value_noise2D_buffers(cl_context context) {
+std::vector<cl_mem> value_noise2D_buffers(cl_context context) {
 	int NUM_ENTRIES = 1024;
-	vector<cl_mem> memobjs(2);
+	std::vector<cl_mem> memobjs(2);
 	memobjs.push_back(clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * 2 * NUM_ENTRIES, NULL, NULL));
 	memobjs.push_back(clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float) * 2 * NUM_ENTRIES, NULL, NULL));
 	return memobjs;
@@ -246,10 +192,9 @@ vector<cl_mem> value_noise2D_buffers(cl_context context) {
 INSTANTIATE_TEST_SUITE_P(opencl, OpenCL_Context_Fixture,
 		Values(OpenCL_Context {"value_noise2D_test", R"EOT(
 #define USE_OPENCL
-__kernel void value_noise2D_test(__global float2 *in, __global float2 *out,
-                          __local float *sMemx, __local float *sMemy) {
-	int tid = get_local_id(0);
-	int x = fast_floor(1.0);
+global int globalA;
+kernel void updateGlobal() {
+	globalA = 75;
 }
 )EOT", &value_noise2D_buffers})
 		);
---
+++ anlopencl/src/test/cpp/program_ex.hpp
@@ -0,0 +1,89 @@
+/*
+ * program_ex.hpp
+ *
+ *  Created on: Jul 29, 2021
+ *      Author: Erwin Müller
+ */
+
+#ifndef PROGRAM_EX_HPP_
+#define PROGRAM_EX_HPP_
+
+#include <CL/cl2.hpp>
+
+using namespace cl;
+
+#define COMPILE_PROGRAM_ERR "Error compile program"
+
+class ProgramEx : public Program {
+public:
+	ProgramEx(const string &source, bool build = false, cl_int *err = NULL) :
+			Program { source, build, err } {
+	}
+
+	ProgramEx(const Context &context, const string &source, bool build = false,
+			cl_int *err = NULL) :
+			Program { source, build, err } {
+	}
+
+	ProgramEx(const Sources &sources, cl_int *err = NULL) : Program { sources, err } {
+	}
+
+	ProgramEx(const Context &context, const Sources &sources,
+			cl_int *err = NULL) :
+			Program { context, sources, err } {
+	}
+
+#if CL_HPP_TARGET_OPENCL_VERSION >= 210 || (CL_HPP_TARGET_OPENCL_VERSION==200 && defined(CL_HPP_USE_IL_KHR))
+	/**
+	 * Program constructor to allow construction of program from SPIR-V or another IL.
+	 * Valid for either OpenCL >= 2.1 or when CL_HPP_USE_IL_KHR is defined.
+	 */
+	ProgramEx(const vector<char> &IL, bool build = false, cl_int *err = NULL) :
+			Program { IL, build, err } {
+	}
+
+	/**
+	 * Program constructor to allow construction of program from SPIR-V or another IL
+	 * for a specific context.
+	 * Valid for either OpenCL >= 2.1 or when CL_HPP_USE_IL_KHR is defined.
+	 */
+	ProgramEx(const Context &context, const vector<char> &IL,
+			bool build = false, cl_int *err = NULL) :
+			Program { context, IL, build, err } {
+	}
+#endif // #if CL_HPP_TARGET_OPENCL_VERSION >= 210
+
+#if CL_HPP_TARGET_OPENCL_VERSION >= 120
+    cl_int compile(
+    		vector<Program> input_headers,
+			vector<string> input_header_names,
+			const char* options = NULL,
+			void (CL_CALLBACK * notifyFptr)(cl_program, void *) = NULL,
+			void* data = NULL) const {
+    	vector<cl_program> programs(input_headers.size());
+        for (unsigned int i = 0; i < input_headers.size(); i++) {
+            programs[i] = input_headers[i]();
+        }
+        size_type n = input_header_names.size();
+        vector<const char*> input_header_names_s(n);
+        for (size_type i = 0; i < n; ++i) {
+        	input_header_names_s[i] = input_header_names[i].data();
+        }
+        cl_int error = ::clCompileProgram(
+            object_,
+            0,
+            NULL,
+            options,
+			programs.size(),
+			programs.data(),
+			input_header_names_s.data(),
+            notifyFptr,
+            data);
+        return detail::buildErrHandler(error, COMPILE_PROGRAM_ERR,
+				getBuildInfo<CL_PROGRAM_BUILD_LOG>());
+	}
+#endif // CL_HPP_TARGET_OPENCL_VERSION >= 120
+
+};
+
+#endif /* PROGRAM_EX_HPP_ */
