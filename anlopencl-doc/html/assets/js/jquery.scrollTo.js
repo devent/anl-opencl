1ebe6751574f07cc3d0cf084e5d66aac7dc1664f
Erwin Müller <erwin.mueller@deventm.org>
2021-11-30 11:01:17 +0100

    Feature #4544 Add license of RandomCL

--- anlopencl-doc/html/assets/js/jquery.scrollTo.js
+++ anlopencl-doc/html/assets/js/jquery.scrollTo.js
@@ -1,11 +1,66 @@
-/**
- * jQuery.ScrollTo - Easy element scrolling using jQuery.
- * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
- * Dual licensed under MIT and GPL.
- * Date: 5/25/2009
- * @author Ariel Flesler
- * @version 1.4.2
+/*
+ * Copyright (C) 2021 Erwin Müller <erwin@muellerpublic.de>
+ * Released as open-source under the Apache License, Version 2.0.
  *
- * http://flesler.blogspot.co.at/2009/05/jqueryscrollto-142-released.html
+ * ****************************************************************************
+ * ANL-OpenCL :: Bundle POM
+ * ****************************************************************************
+ *
+ * Copyright (C) 2021 Erwin Müller <erwin@muellerpublic.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ****************************************************************************
+ * ANL-OpenCL :: Bundle POM is a derivative work based on Josua Tippetts' C++ library:
+ * http://accidentalnoise.sourceforge.net/index.html
+ * ****************************************************************************
+ *
+ * Copyright (C) 2011 Joshua Tippetts
+ *
+ *   This software is provided 'as-is', without any express or implied
+ *   warranty.  In no event will the authors be held liable for any damages
+ *   arising from the use of this software.
+ *
+ *   Permission is granted to anyone to use this software for any purpose,
+ *   including commercial applications, and to alter it and redistribute it
+ *   freely, subject to the following restrictions:
+ *
+ *   1. The origin of this software must not be misrepresented; you must not
+ *      claim that you wrote the original software. If you use this software
+ *      in a product, an acknowledgment in the product documentation would be
+ *      appreciated but is not required.
+ *   2. Altered source versions must be plainly marked as such, and must not be
+ *      misrepresented as being the original software.
+ *   3. This notice may not be removed or altered from any source distribution.
+ *
+ *
+ * ****************************************************************************
+ * ANL-OpenCL :: Bundle POM bundles and uses the RandomCL library:
+ * https://github.com/bstatcomp/RandomCL
+ * ****************************************************************************
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2018, Tadej Ciglarič, Erik Štrumbelj, Rok Češnovar. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 ;(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||a.ownerDocument||a;return d.browser.safari||e.compatMode=='BackCompat'?e.body:e.documentElement})};d.fn.scrollTo=function(n,j,b){if(typeof j=='object'){b=j;j=0}if(typeof b=='function')b={onAfter:b};if(n=='max')n=9e9;b=d.extend({},k.defaults,b);j=j||b.speed||b.duration;b.queue=b.queue&&b.axis.length>1;if(b.queue)j/=2;b.offset=p(b.offset);b.over=p(b.over);return this._scrollable().each(function(){var q=this,r=d(q),f=n,s,g={},u=r.is('html,body');switch(typeof f){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(f)){f=p(f);break}f=d(f,this);case'object':if(f.is||f.style)s=(f=d(f)).offset()}d.each(b.axis.split(''),function(a,i){var e=i=='x'?'Left':'Top',h=e.toLowerCase(),c='scroll'+e,l=q[c],m=k.max(q,i);if(s){g[c]=s[h]+(u?0:l-r.offset()[h]);if(b.margin){g[c]-=parseInt(f.css('margin'+e))||0;g[c]-=parseInt(f.css('border'+e+'Width'))||0}g[c]+=b.offset[h]||0;if(b.over[h])g[c]+=f[i=='x'?'width':'height']()*b.over[h]}else{var o=f[h];g[c]=o.slice&&o.slice(-1)=='%'?parseFloat(o)/100*m:o}if(/^\d+$/.test(g[c]))g[c]=g[c]<=0?0:Math.min(g[c],m);if(!a&&b.queue){if(l!=g[c])t(b.onAfterFirst);delete g[c]}});t(b.onAfter);function t(a){r.animate(g,j,b.easing,a&&function(){a.call(this,n,b)})}}).end()};k.max=function(a,i){var e=i=='x'?'Width':'Height',h='scroll'+e;if(!d(a).is('html,body'))return a[h]-d(a)[e.toLowerCase()]();var c='client'+e,l=a.ownerDocument.documentElement,m=a.ownerDocument.body;return Math.max(l[h],m[h])-Math.min(l[c],m[c])};function p(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
--- anlopencl-jme3-opencl/src/test/groovy/com/anrisoftware/anlopencl/jme/opencl/AnlKernelTest.groovy
+++ anlopencl-jme3-opencl/src/test/groovy/com/anrisoftware/anlopencl/jme/opencl/AnlKernelTest.groovy
@@ -42,6 +42,26 @@
  *   2. Altered source versions must be plainly marked as such, and must not be
  *      misrepresented as being the original software.
  *   3. This notice may not be removed or altered from any source distribution.
+ *
+ *
+ * ****************************************************************************
+ * ANL-OpenCL :: JME3 - OpenCL bundles and uses the RandomCL library:
+ * https://github.com/bstatcomp/RandomCL
+ * ****************************************************************************
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2018, Tadej Ciglarič, Erik Štrumbelj, Rok Češnovar. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.anrisoftware.anlopencl.jme.opencl
 
@@ -517,7 +537,6 @@ write_only image2d_t output
 
 kernel void map2d_image(
 global struct SMappingRanges *g_ranges,
-global kiss09_state *g_srnd,
 write_only image2d_t output
 ) {
     $insert_localMapRange
@@ -537,9 +556,6 @@ write_only image2d_t output
             def err = s.mallocInt(1)
             def ranges = MappingRanges.createWithBuffer(s).setDefault()
             def cl_ranges = new LwjglBuffer(ranges.getClBuffer(s, clcontext))
-            def srnd = Kiss09State.createWithBuffer(s)
-            Kiss09Random.kiss09_seed(srnd, 200);
-            def cl_srnd = new LwjglBuffer(srnd.getClBuffer(s, clcontext))
             int size = width * height
             def format = CLImageFormat.calloc(s)
             format.image_channel_order(CL_RGBA)
@@ -549,7 +565,7 @@ write_only image2d_t output
             checkCLError(err.get(0))
             def global = new WorkSize(width, height)
             def local = new WorkSize(2, 2)
-            def event = anlKernel.run2("map2d_image", queue, global, local, cl_ranges, cl_srnd, cl_output)
+            def event = anlKernel.run2("map2d_image", queue, global, local, cl_ranges, cl_output)
             //def event = anlKernel.run1("map2d_image", queue, global, rangesb, z, dim, output)
             event.waitForFinished()
 
